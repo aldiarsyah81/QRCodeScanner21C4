e9b3e5f99c840efef9f79e1371040f5d618a120f
aldiarsyah81 <aldiarsyah81@gmail.com>
2023-01-08 16:13:38 +0700

    *added scan email

--- app/src/main/java/com/example/qrcodescanner21c4/MainActivity.java
+++ app/src/main/java/com/example/qrcodescanner21c4/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.qrcodescanner21c4;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -10,6 +8,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
@@ -43,51 +43,77 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         buttonScanning.setOnClickListener(this);
     }
     @Override
-    protected  void onActivityResult(int requestCode, int resultCode, Intent data){
-        IntentResult result = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
-        if (result != null){
-            //jika hasil scanner tidak sama sekali
-            if (result.getContents() == null){
-                Toast.makeText(this, "Hasil Scan tidak ada", Toast.LENGTH_LONG).show();
-            }
-            //webview
-            else if (Patterns.WEB_URL.matcher(result.getContents()).matches()) {
-                Intent visitUrl = new Intent(Intent.ACTION_VIEW, Uri.parse(result.getContents()));
-                startActivity(visitUrl);
-            }
-            // geolokasi
-            else if (result.getContents().startsWith("geo:")) {
-                String geoUri = result.getContents();
-                Intent geoIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(geoUri));
-                startActivity(geoIntent);
+    protected  void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Toast.makeText(this, "Not Scanned", Toast.LENGTH_SHORT).show();
             }
             else {
                 // JSON
                 try {
-
                     JSONObject jsonObject = new JSONObject(result.getContents());
                     textViewName.setText(jsonObject.getString("nama"));
                     textViewClass.setText(jsonObject.getString("kelas"));
                     textViewId.setText(jsonObject.getString("nim"));
 
-                } catch (JSONException e){
+                }  catch (JSONException e){
                     e.printStackTrace();
-                    Toast.makeText(this, result.getContents(), Toast.LENGTH_LONG).show();
                 }
-                // DIAL UP, NOMOR TELEPON
+
+                // dial
                 try {
                     Intent intent2 = new Intent(Intent.ACTION_DIAL, Uri.parse(result.getContents()));
                     startActivity(intent2);
                 } catch (Exception e){
-                    Toast.makeText(this, "Not Scanned", Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
                 }
+
+                // QR code geografis
+                try {
+                    String geoUri = result.getContents();
+                    Intent geoIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(geoUri));
+                    startActivity(geoIntent);
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+
+                // email
+                try{
+                    String scannedContent = result.getContents();
+                    // mengecek
+                    if (scannedContent.contains("@")) {
+                        // mengirim
+                        Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                                "mailto", scannedContent.replace("http://", ""), null));
+                        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "QR Code Scanner");
+                        emailIntent.putExtra(Intent.EXTRA_TEXT, "Halo, ini email yang dihasilkan dari QR Code Scanner.");
+                        startActivity(Intent.createChooser(emailIntent, "Send email..."));
+                    } else {
+                        // menampilkan
+                        Toast.makeText(this, "Scanned : " + scannedContent, Toast.LENGTH_SHORT).show();
+                    }
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+
+                Toast.makeText(this, "Scanned : " + result.getContents(), Toast.LENGTH_SHORT).show();
             }
-        }else {
+
+            // webview
+            if (Patterns.WEB_URL.matcher(result.getContents()).matches()) {
+                Intent visitUrl = new Intent(Intent.ACTION_VIEW, Uri.parse(result.getContents()));
+                startActivity(visitUrl);
+            }
+
+        } else {
             super.onActivityResult(requestCode, resultCode, data);
         }
     }
+
     @Override
-    public void onClick(View v) {
+    public void onClick(View view) {
         qrScan.initiateScan();
     }
 }
